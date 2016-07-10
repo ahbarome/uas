4744d01cc4f93580e63006abfd0f911a5c05cae8
Agustín Barona <byes0811@gmail.com>
2016-07-10 14:10:13 -0500

    Unit test project added for DAL component

---
+++ test/Core/DAL/UAS.Core.DALTest/Persisters/UserPersisterTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UAS.Core.DAL.Persisters;
+
+namespace UAS.Core.DALTest
+{
+    [TestClass]
+    public class UserPersisterTest
+    {
+        [TestMethod]
+        public void GetUserByUsernameAndPasswordTest()
+        {
+            var persister = new UserPersister();
+            var user = persister.GetUserByUsernameAndPassword("", "");
+            Assert.IsNotNull(user);
+        }
+    }
+}
