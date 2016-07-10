6682eccab1582e32324a9fe3dcfd46e17dd980ab
Agustín Barona <byes0811@gmail.com>
2016-07-10 13:13:37 -0500

    Added security project and DAL project

---
+++ src/Core/DAL/UAS.Core.DAL/BaseContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UAS.Core.DAL.Common.Model;
+
+namespace UAS.Core.DAL
+{
+    public class BaseContext
+    {
+        private Entities _entities;
+
+        protected Entities Entities { get { return _entities; } set { value = _entities; } }
+
+        public BaseContext()
+        {
+            try
+            {
+                this._entities = new Entities();
+            }
+            catch (Exception exception)
+            {
+            }
+        }
+    }
+}
