052edba700c8cd7d47446d199785e72c9769b583
Agustín Barona <ahbarome@gmail.com>
2016-07-12 16:15:06 -0500

    Menu dynamic added and dummy data in dashboards and virtual classroom modules

---
+++ src/Core/Web/UAS.Core.Web.WebApp/Controllers/AttendanceController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace UAS.Core.Web.WebApp.Controllers
+{
+    public class AttendanceController : SessionController
+    {
+        public ActionResult VirtualStudentsClassRoom()
+        {
+            return View();
+        }
+
+        public ActionResult VirtualTeachersClassRoom()
+        {
+            return View();
+        }
+    }
+}
