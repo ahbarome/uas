ca1278eef4c3e1e2527f33405722435779b2b008
Agustín Barona <ahbarome@gmail.com>
2016-07-13 16:34:29 -0500

    SqlDependency object added and new property in the ConfigurationManager class

--- src/Core/Configuration/UAS.Core.Configuration/ConfigurationManager.cs
+++ src/Core/Configuration/UAS.Core.Configuration/ConfigurationManager.cs
@@ -7,5 +7,19 @@ namespace UAS.Core.Configuration
         public const string SESSION_KEY = "USER_SESSION_KEY";
 
         public const string SECURITY_KEY = "USER_SESSION_KEY";
+
+        public static string ConnectionStringUASEntites
+        {
+            get
+            {
+                return GetConnectionString("UASEntities");
+            }
+        }
+
+        private static string GetConnectionString(string key)
+        {
+            var connectionString = System.Configuration.ConfigurationManager.ConnectionStrings[key].ConnectionString;
+            return connectionString;
+        }
     }
 }
--- src/Core/Web/UAS.Core.Web.WebApp/Global.asax.cs
+++ src/Core/Web/UAS.Core.Web.WebApp/Global.asax.cs
@@ -1,19 +1,31 @@
-﻿using System.Web.Http;
+﻿using System.Data.Entity.Core.EntityClient;
+using System.Data.SqlClient;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using UAS.Core.Configuration;
 
 namespace UAS.Core.Web.WebApp
 {
     public class WebApiApplication : System.Web.HttpApplication
     {
+        private static EntityConnectionStringBuilder entityConnectionString = new EntityConnectionStringBuilder(ConfigurationManager.ConnectionStringUASEntites);
+
         protected void Application_Start()
         {
+            var connectionString = entityConnectionString.ConnectionString;
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            SqlDependency.Start(connectionString);
+        }
+
+        protected void Application_End() {
+            var connectionString = entityConnectionString.ConnectionString;
+            SqlDependency.Stop(connectionString);
         }
     }
 }
