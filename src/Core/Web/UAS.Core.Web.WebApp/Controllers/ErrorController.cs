40cad3bc88be78e88156f1ba4b99e7727d41a641
Agustín Barona <ahbarome@gmail.com>
2016-09-19 17:41:54 -0500

    Added controller to with default pages for errors

---
+++ src/Core/Web/UAS.Core.Web.WebApp/Controllers/ErrorController.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+
+namespace UAS.Core.Web.WebApp.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: Error
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult NotFound()
+        {
+            return View();
+        }
+
+        public ActionResult BadRequest()
+        {
+            return View();
+        }
+    }
+}
--- src/Core/Web/UAS.Core.Web.WebApp/Global.asax.cs
+++ src/Core/Web/UAS.Core.Web.WebApp/Global.asax.cs
@@ -1,4 +1,6 @@
-﻿using System.Data.Entity.Core.EntityClient;
+﻿using log4net;
+using System;
+using System.Data.Entity.Core.EntityClient;
 using System.Data.SqlClient;
 using System.Web.Http;
 using System.Web.Mvc;
@@ -10,22 +12,52 @@ namespace UAS.Core.Web.WebApp
 {
     public class WebApiApplication : System.Web.HttpApplication
     {
+        /// <summary>
+        /// Logger for the controller
+        /// </summary>
+        private static readonly ILog _logger =
+            LogManager.GetLogger(
+                System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        /// <summary>
+        /// Builder for the connection strign
+        /// </summary>
         private static EntityConnectionStringBuilder entityConnectionString = new EntityConnectionStringBuilder(ConfigurationManager.ConnectionStringUASEntites);
 
+        private static string _connectionString = entityConnectionString.ProviderConnectionString;
+
+        /// <summary>
+        /// Initialize the componentes requireds in the application
+        /// </summary>
         protected void Application_Start()
         {
-            var connectionString = entityConnectionString.ProviderConnectionString;
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            SqlDependency.Start(connectionString);
+            SqlDependency.Start(_connectionString);
         }
 
-        protected void Application_End() {
+        /// <summary>
+        /// Stops the components 
+        /// </summary>
+        protected void Application_End()
+        {
             var connectionString = entityConnectionString.ProviderConnectionString;
-            SqlDependency.Stop(connectionString);
+            SqlDependency.Stop(_connectionString);
+        }
+
+        /// <summary>
+        /// Capture global exceptions
+        /// </summary>
+        /// <param name="sender">Sender of the event</param>
+        /// <param name="errorEvent">Event</param>
+        protected void Application_Error(object sender, EventArgs errorEvent)
+        {
+            var raisedException = Server.GetLastError();
+            // Process exception
+            _logger.Fatal(raisedException.Message, raisedException);
         }
     }
 }
